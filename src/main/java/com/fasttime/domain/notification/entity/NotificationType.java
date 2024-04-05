4bcdd75fa8413a9ce895e568eaa8131cbbb03747
JeongUijeong <jeong275117@gmail.com>
2024-04-05 22:34:02 +0900

    [refactor] 알림 종류 수정

--- src/main/java/com/fasttime/domain/notification/entity/NotificationType.java
+++ src/main/java/com/fasttime/domain/notification/entity/NotificationType.java
@@ -2,7 +2,7 @@ package com.fasttime.domain.notification.entity;
 
 public enum NotificationType {
     STUDY_APPLICATION,
-    STUDY_REQUEST,
+    STUDY_SUGGEST,
     STUDY_APPROVE,
     STUDY_REJECT
 }
