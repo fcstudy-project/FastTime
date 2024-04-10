3cfc18e64d722e0e09c5f56da00bc6c9e679ed6e
JeongUijeong <jeong275117@gmail.com>
2024-04-10 23:29:14 +0900

    [refactor] 알림 타입 별 메시지 추가

--- src/main/java/com/fasttime/domain/notification/entity/NotificationType.java
+++ src/main/java/com/fasttime/domain/notification/entity/NotificationType.java
@@ -1,8 +1,17 @@
 package com.fasttime.domain.notification.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum NotificationType {
-    STUDY_APPLICATION,
-    STUDY_SUGGEST,
-    STUDY_APPROVE,
-    STUDY_REJECT
+    STUDY_APPLICATION("새로운 스터디 지원이 들어왔습니다."),
+    STUDY_SUGGEST("새로운 스터디 참여 제안이 있습니다."),
+    STUDY_APPROVE("스터디 참여가 승인되었습니다."),
+    STUDY_REJECT("스터디 참여가 거절되었습니다.");
+
+    private final String message;
+
+    NotificationType(String message) {
+        this.message = message;
+    }
 }
