fa02e12f1964ad13ce7e4392399d640faa005a4b
JeongUijeong <jeong275117@gmail.com>
2024-04-12 21:05:03 +0900

    [refactor] 알림 Entity JavaDoc 작성 및 알림 종류 enum에 대한 주석 추가

--- src/main/java/com/fasttime/domain/notification/entity/Notification.java
+++ src/main/java/com/fasttime/domain/notification/entity/Notification.java
@@ -19,32 +19,65 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+/**
+ * 알림 Entity Class
+ *
+ * @author JeongUijeong (jeong275117@gmail.com)
+ */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Notification extends BaseTimeEntity {
 
+    /**
+     * 알림 식별자
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * 알림 종류: 스터디 참여 지원/제안/승인/거절
+     */
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private NotificationType type;
 
+    /**
+     * 알림 내용
+     */
     @Column(nullable = false)
     private String content;
 
+    /**
+     * 알림과 관련된 URL
+     */
     private String url;
 
+    /**
+     * 알림 확인 여부
+     */
     @Column(nullable = false)
     private boolean isRead;
 
+    /**
+     * 알림 수신 회원 Entity
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Member receiver;
 
+    /**
+     * 알림 Entity Builder
+     *
+     * @param id       알림 식별자
+     * @param type     알림 종류
+     * @param content  알림 내용
+     * @param url      알림과 관련된 URL
+     * @param isRead   알림 확인 여부
+     * @param receiver 알림 수신 회원 Entity
+     */
     @Builder
     public Notification(
         Long id,
--- src/main/java/com/fasttime/domain/notification/entity/NotificationType.java
+++ src/main/java/com/fasttime/domain/notification/entity/NotificationType.java
@@ -2,13 +2,34 @@ package com.fasttime.domain.notification.entity;
 
 import lombok.Getter;
 
+/**
+ * 알림 종류 enum
+ */
 @Getter
 public enum NotificationType {
+    /**
+     * 스터디 참여 지원
+     */
     STUDY_APPLICATION("새로운 스터디 지원이 들어왔습니다."),
+
+    /**
+     * 스터디 참여 제안
+     */
     STUDY_SUGGEST("새로운 스터디 참여 제안이 있습니다."),
+
+    /**
+     * 스터디 참여 승인
+     */
     STUDY_APPROVE("스터디 참여가 승인되었습니다."),
+
+    /**
+     * 스터디 참여 거절
+     */
     STUDY_REJECT("스터디 참여가 거절되었습니다.");
 
+    /**
+     * 알림 종류에 맞는 알림 메시지
+     */
     private final String message;
 
     NotificationType(String message) {
