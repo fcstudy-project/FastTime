75d9192e95adef962455c69080edc2740d7a8da4
KwonJuHwan <118177454+KwonJuHwan@users.noreply.github.com>
2024-04-13 18:09:16 +0900

    [feat] 스터디 테이블 ddl 추가

---
+++ src/main/resources/db/migration/V24__Add_study_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE study
+(
+    id               BIGINT AUTO_INCREMENT NOT NULL,
+    title               VARCHAR(255) NOT NULL,
+    content             VARCHAR(255) NOT NULL,
+    skill               VARCHAR(255) NOT NULL,
+    total               INT          NOT NULL,
+    current             INT          NOT NULL,
+    applicant           INT          NOT NULL,
+    recruitment_start   DATE          NOT NULL,
+    recruitment_end     DATE          NOT NULL,
+    progress_start      DATE          NOT NULL,
+    progress_end        DATE          NOT NULL,
+    contact             VARCHAR(255) NOT NULL,
+    studyState          enum ('DURING','CLOSED') NOT NULL,
+    CONSTRAINT pk_study PRIMARY KEY (id)
+);
+
+ALTER TABLE study
+    ADD CONSTRAINT FK_STUDY_ON_MEMBER FOREIGN KEY (member_id) REFERENCES member (id);
