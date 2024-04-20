9fc24da2c559198c9677674b8dfe743ac9923506
kwonjuhwan <kjh307ok@gmail.com>
2024-04-20 15:36:33 +0900

    [feat] 카테고리 전체 조회를 위한 서비스 로직 추가

--- src/main/java/com/fasttime/domain/study/service/CategoryService.java
+++ src/main/java/com/fasttime/domain/study/service/CategoryService.java
@@ -1,11 +1,14 @@
 package com.fasttime.domain.study.service;
 
+import com.fasttime.domain.study.dto.CategoryResponse;
 import com.fasttime.domain.study.entity.Category;
 import com.fasttime.domain.study.exception.CategoryNotFoundException;
 import com.fasttime.domain.study.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +20,9 @@ public class CategoryService {
     public Category getCategory(Long categoryId) {
         return categoryRepository.findById(categoryId)
             .orElseThrow(CategoryNotFoundException::new);
+    }
+    public List<CategoryResponse> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream().map(CategoryResponse::of).toList();
     }
 }
