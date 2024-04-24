e51e42c5aa0136663317824d3150c024b69138df
JeongUijeong <jeong275117@gmail.com>
2024-04-24 23:55:17 +0900

    [feat] 스터디 지원 API 컨트롤러 구현

---
+++ src/main/java/com/fasttime/domain/study/controller/StudyApplicationController.java
@@ -0,0 +1,41 @@
+package com.fasttime.domain.study.controller;
+
+import com.fasttime.domain.study.dto.request.ApplyToStudyRequestDto;
+import com.fasttime.domain.study.dto.response.ApplyToStudyResponseDto;
+import com.fasttime.domain.study.service.StudyApplicationService;
+import com.fasttime.global.util.ResponseDTO;
+import com.fasttime.global.util.SecurityUtil;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/studies")
+@RequiredArgsConstructor
+@Validated
+public class StudyApplicationController {
+
+    private final SecurityUtil securityUtil;
+    private final StudyApplicationService studyApplicationService;
+
+    @PostMapping("/{studyId}")
+    public ResponseEntity<ResponseDTO<ApplyToStudyResponseDto>> applyToStudy(
+        @PathVariable @NotNull Long studyId,
+        @RequestBody ApplyToStudyRequestDto applyToStudyRequestDto
+    ) {
+        long memberId = securityUtil.getCurrentMemberId();
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(ResponseDTO.res(HttpStatus.CREATED,
+                "성공적으로 스터디 참여를 신청했습니다.",
+                studyApplicationService.apply(studyId, memberId, applyToStudyRequestDto)
+            ));
+    }
+}
