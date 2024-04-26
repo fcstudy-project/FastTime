bdddaf3835c87ac7d079bb0697d288c798cede0b
JeongUijeong <jeong275117@gmail.com>
2024-04-26 21:04:56 +0900

    [feat] 스터디 참여 제안 레포지토리 생성

---
+++ src/main/java/com/fasttime/domain/study/repository/StudySuggestionRepository.java
@@ -0,0 +1,8 @@
+package com.fasttime.domain.study.repository;
+
+import com.fasttime.domain.study.entity.StudySuggestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudySuggestionRepository extends JpaRepository<StudySuggestion, Long> {
+
+}
