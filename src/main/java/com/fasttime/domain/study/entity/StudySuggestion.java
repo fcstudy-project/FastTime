cc444766f22f030e2e2d4b9c0f44ccb0f70f10fb
JeongUijeong <jeong275117@gmail.com>
2024-04-26 20:59:21 +0900

    [feat] 스터디 참여 제안 Entity 정의

---
+++ src/main/java/com/fasttime/domain/study/entity/StudySuggestion.java
@@ -0,0 +1,61 @@
+package com.fasttime.domain.study.entity;
+
+import com.fasttime.domain.member.entity.Member;
+import com.fasttime.global.common.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class StudySuggestion extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StudyRequestStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member receiver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Study study;
+
+    private String message;
+
+    @Builder
+    public StudySuggestion(
+        Long id,
+        StudyRequestStatus status,
+        Member receiver,
+        Study study,
+        String message
+    ) {
+        this.id = id;
+        this.status = status;
+        this.receiver = receiver;
+        this.study = study;
+        this.message = message;
+    }
+}
