cb3212e45fcee70f01545e81994c8ac5b96694ce
JeongUijeong <jeong275117@gmail.com>
2024-04-30 22:28:12 +0900

    [feat] 스터디 참여 신청 API 컨트롤러 추가 구현

--- src/main/java/com/fasttime/domain/study/controller/StudyApplicationController.java
+++ src/main/java/com/fasttime/domain/study/controller/StudyApplicationController.java
@@ -2,6 +2,7 @@ package com.fasttime.domain.study.controller;
 
 import com.fasttime.domain.study.dto.request.ApplyToStudyRequestDto;
 import com.fasttime.domain.study.dto.response.ApplyToStudyResponseDto;
+import com.fasttime.domain.study.dto.response.ApproveStudyApplicationResponseDto;
 import com.fasttime.domain.study.service.StudyApplicationService;
 import com.fasttime.global.util.ResponseDTO;
 import com.fasttime.global.util.SecurityUtil;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,7 +37,20 @@ public class StudyApplicationController {
             .status(HttpStatus.CREATED)
             .body(ResponseDTO.res(HttpStatus.CREATED,
                 "성공적으로 스터디 참여를 신청했습니다.",
-                studyApplicationService.apply(studyId, memberId, applyToStudyRequestDto)
+                studyApplicationService.apply(memberId, studyId, applyToStudyRequestDto)
+            ));
+    }
+
+    @PatchMapping("/{studyId}/applications/{studyApplicationId}")
+    public ResponseEntity<ResponseDTO<ApproveStudyApplicationResponseDto>> approveStudyApplication(
+        @PathVariable @NotNull Long studyApplicationId
+    ) {
+        long memberId = securityUtil.getCurrentMemberId();
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ResponseDTO.res(HttpStatus.OK,
+                "성공적으로 스터디 참여 신청을 승인했습니다.",
+                studyApplicationService.approve(memberId, studyApplicationId)
             ));
     }
 }
