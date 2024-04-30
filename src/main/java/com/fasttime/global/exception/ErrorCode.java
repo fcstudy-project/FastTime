bf9a211986bf1c957b338f06d39c26779ecc72bb
JeongUijeong <jeong275117@gmail.com>
2024-04-30 22:19:42 +0900

    [feat] 존재하지 않는 스터디 신청 에러 코드 추가

--- src/main/java/com/fasttime/global/exception/ErrorCode.java
+++ src/main/java/com/fasttime/global/exception/ErrorCode.java
@@ -73,6 +73,7 @@ public enum ErrorCode {
     CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않는 스터디 분야입니다."),
     HAS_NO_PERMISSION_WITH_THIS_STUDY(HttpStatus.UNAUTHORIZED, "해당 스터디 게시글에 대한 권한이 없습니다."),
     STUDY_DELETED(HttpStatus.NOT_FOUND,"삭제된 스터디 모집글입니다."),
+    STUDY_APPLICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 스터디 신청입니다."),
 
     // SSE & NOTIFICATION
     SSE_CONNECTION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "SSE 연결에 실패했습니다."),
