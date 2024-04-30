5d122c9e468f262c4f5d218a809b61bdfc9d8764
JeongUijeong <jeong275117@gmail.com>
2024-04-30 22:29:40 +0900

    [test] 스터디 참여 신청 API 문서화 컨트롤러 테스트 작성

--- src/test/java/com/fasttime/domain/study/docs/StudyApplicationControllerDocsTest.java
+++ src/test/java/com/fasttime/domain/study/docs/StudyApplicationControllerDocsTest.java
@@ -4,12 +4,15 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,6 +20,7 @@ import com.fasttime.docs.RestDocsSupport;
 import com.fasttime.domain.study.controller.StudyApplicationController;
 import com.fasttime.domain.study.dto.request.ApplyToStudyRequestDto;
 import com.fasttime.domain.study.dto.response.ApplyToStudyResponseDto;
+import com.fasttime.domain.study.dto.response.ApproveStudyApplicationResponseDto;
 import com.fasttime.domain.study.service.StudyApplicationService;
 import com.fasttime.domain.study.service.StudyApplicationServiceImpl;
 import com.fasttime.global.util.SecurityUtil;
@@ -61,6 +65,36 @@ public class StudyApplicationControllerDocsTest extends RestDocsSupport {
                 preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
                 requestFields(
                     fieldWithPath("message").type(JsonFieldType.STRING).description("신청 메시지")),
+                responseFields(
+                    fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 상태코드"),
+                    fieldWithPath("message").type(JsonFieldType.STRING).description("메시지"),
+                    fieldWithPath("data").type(JsonFieldType.OBJECT).description("응답데이터"),
+                    fieldWithPath("data.studyApplicationId").type(JsonFieldType.NUMBER)
+                        .description("스터디 신청 식별자"))));
+    }
+
+    @DisplayName("approve()는 스터디 참여 지원을 승인할 수 있다.")
+    @Test
+    public void approve() throws Exception {
+        // given
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("MEMBER", 1L);
+
+        given(studyApplicationService.approve(
+            any(long.class),
+            any(long.class)
+        )).willReturn(new ApproveStudyApplicationResponseDto(1L));
+
+        // when then
+        mockMvc.perform(patch("/api/v2/studies/{studyId}/applications/{studyApplicationId}", 1L, 1L)
+                .session(session)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andDo(document("study-approve",
+                preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                pathParameters(
+                    parameterWithName("studyId").description("스터디 식별자"),
+                    parameterWithName("studyApplicationId").description("수락할 스터디 참여 신청 식별자")),
                 responseFields(
                     fieldWithPath("code").type(JsonFieldType.NUMBER).description("응답 상태코드"),
                     fieldWithPath("message").type(JsonFieldType.STRING).description("메시지"),
