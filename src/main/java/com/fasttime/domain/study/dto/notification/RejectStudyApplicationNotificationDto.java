00f14c8f33c840976f23f12e4ec62af1c00d69b2
JeongUijeong <jeong275117@gmail.com>
2024-05-01 10:29:15 +0900

    [feat] 스터디 참여 신청 거부 알림 DTO 생성

---
+++ src/main/java/com/fasttime/domain/study/dto/notification/RejectStudyApplicationNotificationDto.java
@@ -0,0 +1,26 @@
+package com.fasttime.domain.study.dto.notification;
+
+import com.fasttime.domain.member.entity.Member;
+import com.fasttime.domain.notification.aop.proxy.NotificationInfo;
+import com.fasttime.domain.notification.entity.NotificationType;
+import com.fasttime.domain.study.entity.StudyApplication;
+
+public record RejectStudyApplicationNotificationDto(
+    StudyApplication studyApplication
+) implements NotificationInfo {
+
+    @Override
+    public Member getReceiver() {
+        return studyApplication.getApplicant();
+    }
+
+    @Override
+    public String getUrl() {
+        return "/api/v2/studies/" + studyApplication.getStudy().getId();
+    }
+
+    @Override
+    public NotificationType getNotificationType() {
+        return NotificationType.STUDY_APPROVE_APPLICATION;
+    }
+}
