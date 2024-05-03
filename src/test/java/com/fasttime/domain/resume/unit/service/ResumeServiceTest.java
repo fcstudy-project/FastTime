3469b5c80507a6a6c1afa272924bdebb6f08638f
Kongji82 <Kongji4092@gmail.com>
2024-05-03 18:22:27 +0900

    [test] 우수 자기소개서 기능 테스트 코드 작성
    - 우수 자기고개서 가져오기 기능 테스트 코드 작성
        - createAt을 설정 못해서 실테스트 적용

--- src/test/java/com/fasttime/domain/resume/unit/service/ResumeServiceTest.java
+++ src/test/java/com/fasttime/domain/resume/unit/service/ResumeServiceTest.java
@@ -24,6 +24,7 @@ import com.fasttime.domain.resume.exception.UnauthorizedAccessLikeException;
 import com.fasttime.domain.resume.repository.LikeRepository;
 import com.fasttime.domain.resume.repository.ResumeRepository;
 import com.fasttime.domain.resume.service.ResumeService;
+import java.util.List;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
@@ -309,7 +310,7 @@ class ResumeServiceTest {
 
         @DisplayName("자기소개서 좋아요 count가 0 이하인 경우 -가 되지 않는다.")
         @Test
-        void test2() {
+        void cancelLike_doNotCalculateMinus() {
             // given
             long memberId = 1L;
             Member member = Member.builder().id(memberId).nickname("testName").build();
@@ -332,6 +333,54 @@ class ResumeServiceTest {
 
             // then
             assertThat(resume.getLikeCount()).isEqualTo(0);
+        }
+    }
+
+    @DisplayName("getBestResume()는")
+    @Nested
+    class Context_getBestResume {
+
+        @DisplayName("성공적으로 최근 2주간 생성된 최고의 게시글 3개를 가져온다.")
+        @Test
+        void _willSuccess() {
+            // given
+            Member member = Member.builder().id(1L).nickname("testName").build();
+
+            // 첫
+            Resume resume1 = Resume.builder()
+                    .title("2nd")
+                    .content("expecting second resume")
+                    .likeCount(2)
+                    .writer(member)
+                    .build();
+
+            Resume resume2 = Resume.builder()
+                    .title("1st")
+                    .content("expecting first resume")
+                    .likeCount(5)
+                    .writer(member)
+                    .build();
+
+            Resume resume3 = Resume.builder()
+                    .title("3rd")
+                    .content("expecting third resume")
+                    .likeCount(1)
+                    .writer(member)
+                    .build();
+
+            given(resumeRepository.getResumesCreatedWithinTwoWeeks()).willReturn(
+                    List.of(resume1, resume2, resume3));
+
+            // when
+            List<ResumeResponseDto> response = resumeService.getBestResume();
+
+            // then
+            assertThat(response.size()).isEqualTo(3);
+            assertThat(response.getFirst()).extracting("title", "content", "likeCount")
+                    .containsExactly("1st", "expecting first resume", 5);
+            assertThat(response.getLast()).extracting("title", "content", "likeCount")
+                    .containsExactly("3rd", "expecting third resume", 1);
+
         }
     }
 
