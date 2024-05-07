0936c59a230896511cb8cb7b78aefd5705b3f7f4
JeongUijeong <jeong275117@gmail.com>
2024-05-07 22:48:27 +0900

    [feat] 스터디 참여 신청 목록 조회 API 컨트롤러 구현

--- src/main/java/com/fasttime/domain/study/controller/StudyApplicationController.java
+++ src/main/java/com/fasttime/domain/study/controller/StudyApplicationController.java
@@ -1,7 +1,10 @@
 package com.fasttime.domain.study.controller;
 
 import com.fasttime.domain.study.dto.request.ApplyToStudyRequestDto;
+import com.fasttime.domain.study.dto.request.GetStudyApplicationsRequestDto;
+import com.fasttime.domain.study.dto.request.StudyApplicationPageRequestDto;
 import com.fasttime.domain.study.dto.response.StudyApplicationResponseDto;
+import com.fasttime.domain.study.dto.response.StudyApplicationsResponseDto;
 import com.fasttime.domain.study.service.StudyApplicationService;
 import com.fasttime.global.util.ResponseDTO;
 import com.fasttime.global.util.SecurityUtil;
@@ -11,11 +14,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,6 +43,30 @@ public class StudyApplicationController {
             .body(ResponseDTO.res(HttpStatus.CREATED,
                 "성공적으로 스터디 참여를 신청했습니다.",
                 studyApplicationService.apply(memberId, studyId, applyToStudyRequestDto)
+            ));
+    }
+
+    @GetMapping("/applications")
+    public ResponseEntity<ResponseDTO<StudyApplicationsResponseDto>> getStudyApplications(
+        @RequestParam(required = false) Long studyId,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int pageSize
+    ) {
+        long memberId = securityUtil.getCurrentMemberId();
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ResponseDTO.res(HttpStatus.OK,
+                "성공적으로 스터디 참여 신청 목록을 조회했습니다.",
+                studyApplicationService.getStudyApplications(
+                    GetStudyApplicationsRequestDto.builder()
+                        .applicantId(memberId)
+                        .studyId(studyId)
+                        .build(),
+                    StudyApplicationPageRequestDto.builder()
+                        .page(page)
+                        .size(pageSize)
+                        .build().of()
+                )
             ));
     }
 
