e7e74bacca26e471d3fba46761acb637bbeaaa21
hym <ghrltjdtprbs@gmail.com>
2024-05-11 18:54:28 +0900

    [refactor] ResumeResponseDto 날짜 포맷을 'yyyy-MM-dd'로 변경
    문제 사항 :
     - 날짜 데이터가 LocalDateTime 형식으로 출력되어 가독성 하락
    
    변경 사항 :
     - @JsonFormat 을 사용하여 ResumeResponseDto 클래스의 날짜 데이터를 'yyyy-MM-dd' 포맷으로 변경하였습니다.
     - JsonFormat 사용을 위해 build.gradle에 관련 의존성을 추가하였습니다.

--- src/main/java/com/fasttime/domain/resume/dto/ResumeResponseDto.java
+++ src/main/java/com/fasttime/domain/resume/dto/ResumeResponseDto.java
@@ -1,23 +1,29 @@
 package com.fasttime.domain.resume.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
 public record ResumeResponseDto(
-        Long id,
-        String title,
-        String content,
-        String writer,
-        int likeCount,
-        int viewCount,
-        LocalDateTime createdAt,
-        LocalDateTime lastModifiedAt,
-        LocalDateTime deletedAt
+    Long id,
+    String title,
+    String content,
+    String writer,
+    int likeCount,
+    int viewCount,
+    @JsonFormat(pattern = "yyyy-MM-dd") LocalDateTime createdAt,
+    @JsonFormat(pattern = "yyyy-MM-dd") LocalDateTime lastModifiedAt,
+    @JsonFormat(pattern = "yyyy-MM-dd") LocalDateTime deletedAt
 ) {
+
     @Override
     public String toString() {
-        return String.format("id: %s, title: %s, content: %s, writer: %s, like count: %d, view count: %d, create at: %s, last modified: %s, delete at: %s",
-                id, title, content, writer, likeCount, viewCount, createdAt, lastModifiedAt, deletedAt != null ? deletedAt : "N/A");
+        return String.format(
+            "id: %s, title: %s, content: %s, writer: %s, like count: %d, view count: %d, created at: %s, last modified: %s, deleted at: %s",
+            id, title, content, writer, likeCount, viewCount,
+            createdAt != null ? createdAt.toString() : "N/A",
+            lastModifiedAt != null ? lastModifiedAt.toString() : "N/A",
+            deletedAt != null ? deletedAt.toString() : "N/A");
     }
 }
