d2197fde05dd0f7bea9ed148790bba50908adb36
JeongUijeong <jeong275117@gmail.com>
2024-05-14 20:39:27 +0900

    [refactor] 스터디 참여 신청/제안 관련 예외 클래스 javadoc 추가

--- src/main/java/com/fasttime/domain/study/exception/NotStudySuggestionReceiverException.java
+++ src/main/java/com/fasttime/domain/study/exception/NotStudySuggestionReceiverException.java
@@ -3,6 +3,9 @@ package com.fasttime.domain.study.exception;
 import com.fasttime.global.exception.ApplicationException;
 import com.fasttime.global.exception.ErrorCode;
 
+/**
+ * 스터디 참여 제안 수신자가 아닌 회원이 참여 제안을 수락하거나 거절하려고 하는 경우에 대한 예외 클래스
+ */
 public class NotStudySuggestionReceiverException extends ApplicationException {
 
     public NotStudySuggestionReceiverException() {
