b6f076389e1d87046ef62420fda102de3b483338
JeongUijeong <jeong275117@gmail.com>
2024-05-14 21:36:08 +0900

    [fix] 오타 수정

--- src/main/resources/db/migration/V35__Add_studySuggestion_table.sql
+++ src/main/resources/db/migration/V35__Add_studySuggestion_table.sql
@@ -8,11 +8,11 @@ CREATE TABLE study_suggestion
     created_at datetime NULL,
     updated_at datetime NULL,
     deleted_at datetime NULL,
-    CONSTRAINT pk_study_application PRIMARY KEY (id)
+    CONSTRAINT pk_study_suggestion PRIMARY KEY (id)
 );
 
 ALTER TABLE study_suggestion
     ADD CONSTRAINT FK_STUDY_SUGGESTION_ON_MEMBER FOREIGN KEY (member_id) REFERENCES member (id);
 
-ALTER TABLE study_application
+ALTER TABLE study_suggestion
     ADD CONSTRAINT FK_STUDY_SUGGESTION_ON_STUDY FOREIGN KEY (study_id) REFERENCES study (id);
