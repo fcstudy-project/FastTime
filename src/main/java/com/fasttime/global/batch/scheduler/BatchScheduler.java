8f533c440e7753edbb001eff54cf43b2197889e6
hym <ghrltjdtprbs@gmail.com>
2024-05-21 22:17:34 +0900

    [fix] BatchScheduler 임시 변경
    문제 사항 :
     - 배치 작업에서 ID 생성 관련 이슈 발생

--- src/main/java/com/fasttime/global/batch/scheduler/BatchScheduler.java
+++ src/main/java/com/fasttime/global/batch/scheduler/BatchScheduler.java
@@ -29,7 +29,7 @@ public class BatchScheduler {
         this.updateResumeViewCountToDbJob = updateResumeViewCountToDbJob;
     }
 
-    @Scheduled(cron = "0 15 22 * * *")
+    @Scheduled(cron = "0 25 22 * * *")
     public void runDeleteOldReviewsJob() throws JobExecutionException {
         jobLauncher.run(deleteOldReviewsJob, new JobParameters());
     }
