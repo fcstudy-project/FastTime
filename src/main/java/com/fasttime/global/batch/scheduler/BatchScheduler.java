df7ff9f18f6e3f724b25b28de8403bce8e580cac
hym <ghrltjdtprbs@gmail.com>
2024-05-21 17:08:07 +0900

    [fix] BATCH_JOB_SEQ 스크립트 수정
    문제 사항 :
     - MySQL에서는 AUTO_INCREMENT 속성이 있는 열이 반드시 기본 키로 지정되어야 하는데, 스크립트에서는 ID_NAME을 기본 키로 설정하고, ID_VAL 열에 AUTO_INCREMENT 속성을 부여했음.
     - ID_VAL이 기본 키로 설정되지 않았기 때문에 에러가 발생
    
    변경 사항 :
     - ID_VAL 열을 기본 키로 설정하고, 필요하다면 ID_NAME을 보조 키로 사용할 수 있도록 변경

--- src/main/java/com/fasttime/global/batch/scheduler/BatchScheduler.java
+++ src/main/java/com/fasttime/global/batch/scheduler/BatchScheduler.java
@@ -29,7 +29,7 @@ public class BatchScheduler {
         this.updateResumeViewCountToDbJob = updateResumeViewCountToDbJob;
     }
 
-    @Scheduled(cron = "0 5 17 * * *")
+    @Scheduled(cron = "0 20 17 * * *")
     public void runDeleteOldReviewsJob() throws JobExecutionException {
         jobLauncher.run(deleteOldReviewsJob, new JobParameters());
     }
